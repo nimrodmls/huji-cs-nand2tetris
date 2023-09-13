59815adc429b045906d5411b75db1803df517ce3
Nimrod Mallis <nimrodmallis@gmail.com>
2023-09-13 14:31:23 +0300

    Implemented pop, docs and finalizing is still required

--- projects/07/CodeWriter.py
+++ projects/07/CodeWriter.py
@@ -30,8 +30,9 @@ M=D\n"""
 # After calling this piece of code, the A register will hold
 # the address requested for the memory segment
 GET_ADDRESS_KEYWORDED_SEGMENT_ASM = \
-"""@{segment}
-D={address}
+"""@{address}
+D=A
+@{segment}
 A=D+M\n"""
 
 class CodeWriter:
@@ -86,7 +87,7 @@ class CodeWriter:
         A=M
         D=M
         A=A-1
-        M=D+M"""
+        M=D+M\n"""
 
     def vm_sub(self) -> str:
         """
@@ -101,7 +102,7 @@ class CodeWriter:
         A=M
         D=M
         A=A-1
-        M=D-M"""
+        M=D-M\n"""
 
     def vm_neg(self) -> str:
         """
@@ -113,7 +114,7 @@ class CodeWriter:
         return """// neg
         @SP
         A=M-1
-        M=-M"""
+        M=-M\n"""
 
     def vm_eq(self) -> str:
         """
@@ -140,7 +141,7 @@ class CodeWriter:
         A=M
         D=M
         A=A-1
-        M=D&M"""
+        M=D&M\n"""
 
     def vm_or(self) -> str:
         return """// and
@@ -149,13 +150,13 @@ class CodeWriter:
         A=M
         D=M
         A=A-1
-        M=D|M"""
+        M=D|M\n"""
 
     def vm_not(self) -> str:
         return """// neg
         @SP
         A=M-1
-        M=!M"""
+        M=!M\n"""
 
     # Stack-manipulating commands
 
@@ -181,12 +182,25 @@ class CodeWriter:
         A=M
         M=D
         @SP
-        M=M+1"""
+        M=M+1\n"""
         return asm_code
 
     def vm_pop(self, segment: str, address: int) -> str:
         """
         """
+        asm_code = f"// pop {segment} {address}\n" + CodeWriter._generate_segment_address(segment, address)
+        #
+        asm_code += """D=A
+            @R15
+            M=D
+            @SP
+            M=M-1
+            A=M
+            D=M
+            @R15
+            A=M
+            M=D\n"""
+        return asm_code
 
         
     @staticmethod
@@ -208,132 +222,3 @@ class CodeWriter:
         """
         ram_offset = offset + address
         return f"@{ram_offset}\n"
-
-    def write_arithmetic(self, command: str) -> None:
-        """Writes assembly code that is the translation of the given 
-        arithmetic command. For the commands eq, lt, gt, you should correctly
-        compare between all numbers our computer supports, and we define the
-        value "true" to be -1, and "false" to be 0.
-
-        Args:
-            command (str): an arithmetic command.
-        """
-        # Your code goes here!
-        pass
-
-    def write_push_pop(self, command: str, segment: str, index: int) -> None:
-        """Writes assembly code that is the translation of the given 
-        command, where command is either C_PUSH or C_POP.
-
-        Args:
-            command (str): "C_PUSH" or "C_POP".
-            segment (str): the memory segment to operate on.
-            index (int): the index in the memory segment.
-        """
-        # Your code goes here!
-        # Note: each reference to "static i" appearing in the file Xxx.vm should
-        # be translated to the assembly symbol "Xxx.i". In the subsequent
-        # assembly process, the Hack assembler will allocate these symbolic
-        # variables to the RAM, starting at address 16.
-        pass
-
-    def write_label(self, label: str) -> None:
-        """Writes assembly code that affects the label command. 
-        Let "Xxx.foo" be a function within the file Xxx.vm. The handling of
-        each "label bar" command within "Xxx.foo" generates and injects the symbol
-        "Xxx.foo$bar" into the assembly code stream.
-        When translating "goto bar" and "if-goto bar" commands within "foo",
-        the label "Xxx.foo$bar" must be used instead of "bar".
-
-        Args:
-            label (str): the label to write.
-        """
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        pass
-    
-    def write_goto(self, label: str) -> None:
-        """Writes assembly code that affects the goto command.
-
-        Args:
-            label (str): the label to go to.
-        """
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        pass
-    
-    def write_if(self, label: str) -> None:
-        """Writes assembly code that affects the if-goto command. 
-
-        Args:
-            label (str): the label to go to.
-        """
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        pass
-    
-    def write_function(self, function_name: str, n_vars: int) -> None:
-        """Writes assembly code that affects the function command. 
-        The handling of each "function Xxx.foo" command within the file Xxx.vm
-        generates and injects a symbol "Xxx.foo" into the assembly code stream,
-        that labels the entry-point to the function's code.
-        In the subsequent assembly process, the assembler translates this 
-        symbol into the physical address where the function code starts.
-
-        Args:
-            function_name (str): the name of the function.
-            n_vars (int): the number of local variables of the function.
-        """
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        # The pseudo-code of "function function_name n_vars" is:
-        # (function_name)       // injects a function entry label into the code
-        # repeat n_vars times:  // n_vars = number of local variables
-        #   push constant 0     // initializes the local variables to 0
-        pass
-    
-    def write_call(self, function_name: str, n_args: int) -> None:
-        """Writes assembly code that affects the call command. 
-        Let "Xxx.foo" be a function within the file Xxx.vm.
-        The handling of each "call" command within Xxx.foo's code generates and
-        injects a symbol "Xxx.foo$ret.i" into the assembly code stream, where
-        "i" is a running integer (one such symbol is generated for each "call"
-        command within "Xxx.foo").
-        This symbol is used to mark the return address within the caller's 
-        code. In the subsequent assembly process, the assembler translates this
-        symbol into the physical memory address of the command immediately
-        following the "call" command.
-
-        Args:
-            function_name (str): the name of the function to call.
-            n_args (int): the number of arguments of the function.
-        """
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        # The pseudo-code of "call function_name n_args" is:
-        # push return_address   // generates a label and pushes it to the stack
-        # push LCL              // saves LCL of the caller
-        # push ARG              // saves ARG of the caller
-        # push THIS             // saves THIS of the caller
-        # push THAT             // saves THAT of the caller
-        # ARG = SP-5-n_args     // repositions ARG
-        # LCL = SP              // repositions LCL
-        # goto function_name    // transfers control to the callee
-        # (return_address)      // injects the return address label into the code
-        pass
-    
-    def write_return(self) -> None:
-        """Writes assembly code that affects the return command."""
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        # The pseudo-code of "return" is:
-        # frame = LCL                   // frame is a temporary variable
-        # return_address = *(frame-5)   // puts the return address in a temp var
-        # *ARG = pop()                  // repositions the return value for the caller
-        # SP = ARG + 1                  // repositions SP for the caller
-        # THAT = *(frame-1)             // restores THAT for the caller
-        # THIS = *(frame-2)             // restores THIS for the caller
-        # ARG = *(frame-3)              // restores ARG for the caller
-        # LCL = *(frame-4)              // restores LCL for the caller
-        # goto return_address           // go to the return address
-        pass
